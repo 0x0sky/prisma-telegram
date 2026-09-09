278b016c40dfe19633bf7f467acd595b0d31e62f
Sasha Lohozinskyy <0x0sky@proton.me>
2026-09-09 17:17:18 +0300

    Adapt shared presenter for Prisma client

---
+++ lib/prisma_telegram/presenter.rb
@@ -0,0 +1,21 @@
+# © 2026 aiaiaiai · aiaiaiai.org
+
+module PrismaTelegram
+  class Presenter < PrismBot::Channels::Telegram::ResultPresenter
+    def help
+      Copy::HELP
+    end
+
+    def started(actor)
+      unless actor.is_a?(PrismBot::Domain::HumanActor)
+        raise ArgumentError, "actor must be a HumanActor"
+      end
+
+      "Prisma готова. Публічний ID: #{actor.canonical_id}.\n\n#{help}"
+    end
+
+    def unknown
+      "Невідома команда.\n\n#{help}"
+    end
+  end
+end
